00a9422b32448c508e9a9df72e4d31d4e26d0127
havannaa <zzz-ivanz@yandex.com>
2026-08-12 22:39:41 +0600

    Update title bar styling: transparent background, small text, and logo removal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types'
 import './Header.css'
-import icon from '../assets/icon.png?inline'
 
 function Header({ title }) {
   return (
     <>
       <title>{title}</title>
       <header>
-        <img src={icon} alt="Cloud Phone logo" draggable="false" width="1em" height="1em" />
         <h1>{title}</h1>
       </header>
     </>
